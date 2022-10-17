622deaecbdd46f9594960c7072e6d665ab09598d
Efrén Fuentes <developer@efrenfuentes.net>
2022-10-16 23:04:11 -0400

    refactor to make json the default format and always show details

--- internal/api/get_ip.go
+++ internal/api/get_ip.go
@@ -10,11 +10,58 @@ import (
 )
 
 func (api *API) GetIpHandler(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		panic(err)
+	ip := getIP(r)
+	if ip == "" {
+		api.ErrorResponse(w, r, http.StatusInternalServerError, "can't get the IP address")
+		return
 	}
 
+	ipinfo_api := ipinfo_client.NewClient()
+	ipinfo_api.SetAccessToken(api.Config.IpinfoAccessToken)
+	details, err := ipinfo_api.GetDetails(ip)
+	if err != nil {
+		api.ServerErrorResponse(w, r, err)
+		return
+	}
+
+	format := get_param(r, "format", "json")
+
+	if format == "json" {
+		var env Envelope
+		
+		env = Envelope{
+			"ip": ip,
+			"city": details.City,
+			"region": details.Region,
+			"country": details.Country,
+			"loc": details.Loc,
+			"org": details.Org,
+			"postal": details.Postal,
+			"timezone": details.Timezone,
+		}
+
+		err = api.WriteJSON(w, http.StatusOK, env, nil)
+		if err != nil {
+			api.ServerErrorResponse(w, r, err)
+		}	
+	} else if format == "text" {
+		var response_body string
+
+		response_body = fmt.Sprintf("Your IP address is: %s\nCity: %s\nRegion: %s\nCountry: %s\nLoc: %s\nOrg: %s\nPostal: %s\nTimezone: %s\n",
+			ip, details.City, details.Region, details.Country, details.Loc, details.Org, details.Postal, details.Timezone)
+
+		err = api.WritePlainText(w, http.StatusOK, response_body, nil)
+		if err != nil {
+			api.ServerErrorResponse(w, r, err)
+		}
+	} else {
+		api.ErrorResponse(w, r, http.StatusBadRequest, "Invalid format")
+	}
+
+	return
+}
+
+func getIP(r *http.Request) string {
 	// We'll always grab the first IP address in the X-Forwarded-For header
 	// list.  We do this because this is always the *origin* IP address, which
 	// is the *true* IP of the user.  For more information on this, see the
@@ -23,61 +70,10 @@ func (api *API) GetIpHandler(w http.ResponseWriter, r *http.Request) {
 
 	// If the IP address is empty, then we'll just return an empty string.
 	if ip == "" {
-		api.ServerErrorResponse(w, r, err)
-		return
+		return ""
 	}
 
-	show_details := get_param(r, "details") == "true"
-
-	var ipinfo_api *ipinfo_client.Client
-	var ip_details *ipinfo_client.Details
-
-	if show_details {
-		ipinfo_api = ipinfo_client.NewClient()
-		ipinfo_api.SetAccessToken(api.Config.IpinfoAccessToken)
-		ip_details, err = ipinfo_api.GetDetails(ip)
-		if err != nil {
-			api.ServerErrorResponse(w, r, err)
-			return
-		}
-	}
-
-	format := get_param(r, "format", "text")
-
-	switch format {
-	case "json":
-		var env Envelope
-	
-		if show_details {
-			env = Envelope{
-				"ip_info": ip_details,
-			}
-		} else {
-			env = Envelope{
-				"ip": ip,
-			}
-		}
-
-		err = api.WriteJSON(w, http.StatusOK, env, nil)
-		if err != nil {
-			api.ServerErrorResponse(w, r, err)
-		}
-		return
-	default:
-		var response_body string
-
-		if show_details {
-			response_body = fmt.Sprintf("Your IP address is: %s\nCity: %s\nRegion: %s\nCountry: %s\nLoc: %s\nOrg: %s\nPostal: %s\nTimezone: %s\n",
-				ip, ip_details.City, ip_details.Region, ip_details.Country, ip_details.Loc, ip_details.Org, ip_details.Postal, ip_details.Timezone)
-		} else {
-			response_body = ip
-		}
-
-		err = api.WritePlainText(w, http.StatusOK, response_body, nil)
-		if err != nil {
-			api.ServerErrorResponse(w, r, err)
-		}
-	}
+	return ip
 }
 
 func get_param(r *http.Request, param string, default_value ...string) string {
