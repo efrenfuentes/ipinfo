91d8d222abf044dc8c3223cc36f634219f8685e8
Efrén Fuentes <developer@efrenfuentes.net>
2022-10-17 12:34:53 -0400

    split endpoints, my_ip for check public ip of caller and ip_info to get info for specific ip

--- internal/api/get_ip_info.go
+++ internal/api/get_ip_info.go
@@ -3,20 +3,19 @@ package api
 import (
 	"net"
 	"net/http"
-	"strings"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/efrenfuentes/ipinfo/internal/ipinfo_client"
 )
 
-func (api *API) GetIpHandler(w http.ResponseWriter, r *http.Request) {
-	// We'll always grab the first IP address in the X-Forwarded-For header
-	// list.  We do this because this is always the *origin* IP address, which
-	// is the *true* IP of the user.  For more information on this, see the
-	// Wikipedia page: https://en.wikipedia.org/wiki/X-Forwarded-For
-	ip := net.ParseIP(strings.Split(r.Header.Get("X-Forwarded-For"), ",")[0]).String()
+func (api *API) GetIpInfoHandler(w http.ResponseWriter, r *http.Request) {
+	// Get the "ip" parameter from the request context.
+	params := httprouter.ParamsFromContext(r.Context())
 
-	if ip == "" {
-		api.ErrorResponse(w, r, http.StatusInternalServerError, "can't get the IP address")
+	ip := params.ByName("ip")
+
+	if net.ParseIP(ip) == nil {
+		api.ErrorResponse(w, r, http.StatusBadRequest, "Invalid IP address")
 		return
 	}
 
--- internal/api/routes.go
+++ internal/api/routes.go
@@ -19,7 +19,8 @@ func (api *API) Routes() http.Handler {
 	// it as the custom error handler for 405 Method Not Allowed responses.
 	router.MethodNotAllowed = http.HandlerFunc(api.MethodNotAllowedResponse)
 
-	router.HandlerFunc(http.MethodGet, "/", api.GetIpHandler)
+	router.HandlerFunc(http.MethodGet, "/my_ip", api.GetIpHandler)
+	router.HandlerFunc(http.MethodGet, "/ip_info/:ip", api.GetIpInfoHandler)
 	router.HandlerFunc(http.MethodGet, "/healthcheck", api.HealthcheckHandler)
 
 	return middleware.EnableCORS(router)
